d87a6f62875fa1ab85700abac080f47d828c64da
schadrack <schadrackbotombe@gmail.com>
2022-02-02 17:25:57 +0200

    updated location

--- components/Location.jsx
+++ components/Location.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 const Location = () => {
   return (
-      <>
-        <h1>Where i work</h1>
+    <Box>
+      <Grid container sx={{ border: "1px solid red", height: "100vh" }}>
+        <Grid item md={6} sx={{ border: "1px solid red", display: "flex",alignItems:"center",justifyContent:"center" }}>
+          <Box
+            sx={{
+              background: `url(map.jpg)`,
+              backgroundSize: "cover",
+              height: "203px",
+              width: "283px",
+              borderRadius:"10px",
+          
+            }}
+          ></Box>
+        </Grid>
+        <Grid item md={6} sx={{ border: "1px solid red" }}>
+          <Box sx={{height:"100px"}>
+          <Typography variant="h6"  >Where i work</Typography>
 
-      </>
-  )
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Location;
