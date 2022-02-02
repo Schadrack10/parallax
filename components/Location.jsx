a96bbd26d59d7e68091bb7f48478f2c4f69b519b
schadrack <schadrackbotombe@gmail.com>
2022-02-02 16:48:54 +0200

    created where i work section

---
+++ components/Location.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Location = () => {
+  return (
+      <>
+        <h1>Where i work</h1>
+
+      </>
+  )
+};
+
+export default Location;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Header from "../components/Header";
+import Location from "../components/Location";
 
 
 export default function Home() {
   return (
     <>   
   
-        <Header/>
+<Location/>
       
     </>
   )
