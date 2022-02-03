6716f4d766ac464410a02f8c3c8f8b54d93348f7
schadrack <schadrackbotombe@gmail.com>
2022-02-03 10:53:06 +0200

    updated my-work

---
+++ components/Location.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+
+const Location = () => {
+  return (
+    <Box>
+      <Grid container sx={{ border: "1px solid red", height: "100vh" }}>
+        <Grid item md={6} sx={{ border: "1px solid red", display: "flex",alignItems:"center",justifyContent:"center" }}>
+          <Box
+            sx={{
+              background: `url(map.jpg)`,
+              backgroundSize: "cover",
+              height: "203px",
+              width: "283px",
+              borderRadius:"10px",
+          
+            }}
+          ></Box>
+        </Grid>
+        <Grid item md={6} sx={{ border: "1px solid red" }}>
+          <Box sx={{height:"100px",border:"1px solid red"}}>
+          <Typography variant="h6"  >Where i worked</Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Location;
